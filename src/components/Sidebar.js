42bfe381ffee6d840985009b1f73008808421a96
HrithikGit <mandhahrithik@gmail.com>
2024-02-14 17:38:53 -0500

    Initial changes

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+function Sidebar(){
+    return (
+        <>
+            <Container>
+                Hello 
+            </Container>
+        </>
+    )
+}
+
+export default Sidebar;
