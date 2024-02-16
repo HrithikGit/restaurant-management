c4550db8b90c53993ada90cfb505a7f95fd15320
kaushikchitipothu <kaushikchitipothu5@gmail.com>
2024-02-16 18:06:51 -0500

    Initial sidebar changes

---
+++ src/components/SidebarData.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/HomeOutlined';
+import MenuIcon from '@mui/icons-material/MenuBookOutlined';
+import OrderIcon from '@mui/icons-material/RestaurantMenuOutlined';
+import CustomerIcon from '@mui/icons-material/PeopleOutlineOutlined';
+import AnalyticsIcon from '@mui/icons-material/BarChartOutlined';
+
+export const SidebarData = [
+    {
+        title: "Dashboard",
+        icon: <HomeIcon/>,
+        link: "/dashboard"
+    },
+    {
+        title: "Orders",
+        icon: <OrderIcon/>,
+        link: "/orders"
+    },
+    {
+        title: "Menus",
+        icon: <MenuIcon/>,
+        link: "/menus",
+        subData: [
+            {
+                title: "Add New Menu",
+                link: "/addMenu"
+            },
+            {
+                title: "Menu List",
+                link: "/menuList"
+            },
+            {
+                title: "Categories",
+                link: "/categories"
+            },
+        ]
+    },
+    {
+        title: "Customer",
+        icon: <CustomerIcon/>,
+        link: "/customer"
+    },
+    {
+        title: "Analytics",
+        icon: <AnalyticsIcon/>,
+        link: "/analytics"
+    },
+    {
+        title: "Settings",
+        icon: <HomeIcon/>,
+        link: "/settings"
+    }
+]
--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Dashboard from './components/Dashboard';
 
-import Sidebar from './components/Sidebar'
-
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />
+  },
+  {
+    path:'/dashboard',
+    element: <Dashboard />
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Sidebar/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
